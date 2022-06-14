81495a0254330a77e50a5189a204d0773328a4c9
luongtruong20201 <luongtruong20201@gmail.com>
2022-06-15 02:54:30 +0700

    Them phan tour cho file index.html

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -2,46 +2,85 @@ tours = [
     {
         heading: "TOUR DU LỊCH ĐÀ NẴNG",
         tours: [
-            { title: "Hành trình Di sản Miền Trung", name: "Đà Nẵng - Hội An - Quảng Trị - Quảng Bình - Huế'", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô" },
-            { title: "Đà Nẵng - Hội An - Cố đô Huế", name: "Đà Nẵng - Hội An - huế", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô" },
-            { title: "Đà Nẵng - Đảo Lý Sơn", name: "Đà Nẵng - Đảo Lý Sơn", price: "Liên hệ để biết thêm", time: "2 Ngày 1 Đêm", vehicle: "Cano, Ô tô" },
-            { title: "Đà Nẵng - Đảo Lý Sơn", name: "Đà Nẵng - Đảo Lý Sơn", price: "Liên hệ để biết thêm", time: "2 Ngày 1 Đêm", vehicle: "Cano, Ô tô" },
-            { title: "Đà Nẵng - Mỹ Sơn - Hội An", name: "Đà Nẵng - Mỹ Sơn - Hội An", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô" },
-            { title: "Đà Nẵng - Hội An - Vinpearl Land Hội An", name: "Đà Nẵng - Hội An - Bà Nà Hills - Vinpearl Land Nam Hội An", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô" },
-            { title: "Đà Nẵng - Hội An - Núi Thần Tài", name: "Đà Nẵng - Hội An - Bà Nà Hills - Núi Thần Tài", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô" },
-            { title: "Đà Nẵng - Hội An - Cù Lao Chàm", name: "Đà Nẵng - Hội An - Bà Nà Hills - Cù Lao Chàm", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô" },
-            { title: "Đà Nẵng - Hội An - Bà Nà Hills", name: "Đà Nẵng - Hội An - Bà Nà Hills", price: "Liên hệ để biết thêm", time: "3 Ngày 2 Đêm", vehicle: "Ô tô" },
-            
+            { title: "Hành trình Di sản Miền Trung", name: "Đà Nẵng - Hội An - Quảng Trị - Quảng Bình - Huế'", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_DaNang/1.jpg" },
+            { title: "Đà Nẵng - Hội An - Cố đô Huế", name: "Đà Nẵng - Hội An - huế", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_DaNang/2.jpg" },
+            { title: "Đà Nẵng - Đảo Lý Sơn", name: "Đà Nẵng - Đảo Lý Sơn", price: "Liên hệ để biết thêm", time: "2 Ngày 1 Đêm", vehicle: "Cano, Ô tô", img: "assets/Img/Tours/Tour_DaNang/3.jpg" },
+            { title: "Đà Nẵng - Đảo Lý Sơn", name: "Đà Nẵng - Đảo Lý Sơn", price: "Liên hệ để biết thêm", time: "2 Ngày 1 Đêm", vehicle: "Cano, Ô tô", img: "assets/Img/Tours/Tour_DaNang/4.jpg" },
+            { title: "Đà Nẵng - Mỹ Sơn - Hội An", name: "Đà Nẵng - Mỹ Sơn - Hội An", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_DaNang/5.jpg" },
+            { title: "Đà Nẵng - Hội An - Vinpearl Land Hội An", name: "Đà Nẵng - Hội An - Bà Nà Hills - Vinpearl Land Nam Hội An", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_DaNang/6.jpg" },
+            { title: "Đà Nẵng - Hội An - Núi Thần Tài", name: "Đà Nẵng - Hội An - Bà Nà Hills - Núi Thần Tài", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_DaNang/7.jpg" },
+            { title: "Đà Nẵng - Hội An - Cù Lao Chàm", name: "Đà Nẵng - Hội An - Bà Nà Hills - Cù Lao Chàm", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_DaNang/8.jpg" },
+            // { title: "Đà Nẵng - Hội An - Bà Nà Hills", name: "Đà Nẵng - Hội An - Bà Nà Hills", price: "Liên hệ để biết thêm", time: "3 Ngày 2 Đêm", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_DaNang/1.jpg" },            
         ]
     },
     {
         heading: "TOUR DU LỊCH TRONG NƯỚC",
         tours: [
-            { title: "Đà Nẵng - Nha Trang - Đà Lạt - Quy Nhơn", name: "Đà Nẵng - Nha Trang - Đà Lạt - Quy Nhơn", price: "Liên hệ để biết thêm", time: "5 Ngày 4 Đêm", vehicle: "Ô tô" },
-            { title: "Đà Nẵng - VQG Bạch Mã", name: "Đà Nẵng - Vườn quốc gia Bạch Mã", price: "Liên hệ để biết thêm", time: "1 Ngày", vehicle: "Ô tô" },
-            { title: "Đà Nẵng - Mỹ Sơn - Hội An", name: "Đà Nẵng - Mỹ Sơn - Hội An", price: "750.000 VNĐ", time: "1 Ngày", vehicle: "Ô tô" },
-            { title: "Thiên  đường nghỉ dưỡng Phú Quốc", name: "Phú Quốc", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay" },
-            { title: "Khám phá Miền Tây", name: "Miền Tây", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay" },
-            { title: "Đà Nẵng - Hà Nội - Ninh Bình", name: "Đà Nẵng - Hà Nội - Ninh Bình", price: "Liên hệ để biết thêm", time: "3 Ngày 2 Đêm", vehicle: "Ô tô - Máy Bay" },
-            { title: "Hà Nội - Hạ Long - Ninh Bình", name: "Đà Nẵng - Hà Nội - Hạ Long - Ninh Bình - Đà Nẵng", price: "Liên hệ để biết thêm", time: "5 Ngày 4 Đêm", vehicle: "Ô tô - Máy Bay" },
-            { title: "Hà Nội - Ninh Bình - Sapa - Hạ Long", name: "Đà Nẵng - Hà Nội - Hạ Long - Ninh Bình - Đà Nẵng", price: "Liên hệ để biết thêm", time: "5 Ngày 4 Đêm", vehicle: "Ô tô - Máy Bay" },
-            { title: "Hà Nội - Ninh Bình - Sapa - Hạ Long", name: "Đà Nẵng - Hà Nội - Tràng An - Sapa - Hạ Long - Đà Nẵng", price: "Liên hệ để biết thêm", time: "6 Ngày 5 Đêm", vehicle: "Ô tô - Máy Bay" },
-            
+            { title: "Đà Nẵng - Nha Trang - Đà Lạt - Quy Nhơn", name: "Đà Nẵng - Nha Trang - Đà Lạt - Quy Nhơn", price: "Liên hệ để biết thêm", time: "5 Ngày 4 Đêm", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_TrongNuoc/1.jpg" },
+            { title: "Đà Nẵng - VQG Bạch Mã", name: "Đà Nẵng - Vườn quốc gia Bạch Mã", price: "Liên hệ để biết thêm", time: "1 Ngày", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_TrongNuoc/2.jpg" },
+            { title: "Đà Nẵng - Mỹ Sơn - Hội An", name: "Đà Nẵng - Mỹ Sơn - Hội An", price: "750.000 VNĐ", time: "1 Ngày", vehicle: "Ô tô", img: "assets/Img/Tours/Tour_TrongNuoc/3.jpg" },
+            { title: "Thiên  đường nghỉ dưỡng Phú Quốc", name: "Phú Quốc", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_TrongNuoc/4.jpg" },
+            { title: "Khám phá Miền Tây", name: "Miền Tây", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_TrongNuoc/5.jpg" },
+            { title: "Đà Nẵng - Hà Nội - Ninh Bình", name: "Đà Nẵng - Hà Nội - Ninh Bình", price: "Liên hệ để biết thêm", time: "3 Ngày 2 Đêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_TrongNuoc/6.jpg" },
+            { title: "Hà Nội - Hạ Long - Ninh Bình", name: "Đà Nẵng - Hà Nội - Hạ Long - Ninh Bình - Đà Nẵng", price: "Liên hệ để biết thêm", time: "5 Ngày 4 Đêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_TrongNuoc/7.jpg" },
+            { title: "Hà Nội - Ninh Bình - Sapa - Hạ Long", name: "Đà Nẵng - Hà Nội - Hạ Long - Ninh Bình - Đà Nẵng", price: "Liên hệ để biết thêm", time: "5 Ngày 4 Đêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_TrongNuoc/8.jpg" },
+            // { title: "Hà Nội - Ninh Bình - Sapa - Hạ Long", name: "Đà Nẵng - Hà Nội - Tràng An - Sapa - Hạ Long - Đà Nẵng", price: "Liên hệ để biết thêm", time: "6 Ngày 5 Đêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_TrongNuoc/1.jpg" },
+
 
         ]
     },
     {
         heading: "TOUR DU LỊCH QUỐC TẾ",
         tours: [
-            { title: "Tour Trung Quốc", name: "Bắc Kinh - Thượng Hải - Hàng Châu - Tô Châu", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay" },
-            { title: "Tour Nhật Bản", name: "Osaka - Nara - Kyoto - Núi Fuji - Tokio", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay" },
-            { title: "Tour Hàn Quốc", name: "Seoul - Nami - Everland", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay" },
-
+            { title: "Tour Trung Quốc", name: "Bắc Kinh - Thượng Hải - Hàng Châu - Tô Châu", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_QuocTe/1.jpg" },
+            { title: "Tour Nhật Bản", name: "Osaka - Nara - Kyoto - Núi Fuji - Tokio", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_QuocTe/2.jpg" },
+            { title: "Tour Hàn Quốc", name: "Seoul - Nami - Everland", price: "Liên hệ để biết thêm", time: "Liên hệ để biết thêm", vehicle: "Ô tô - Máy Bay", img: "assets/Img/Tours/Tour_QuocTe/3.jpg" },
         ]
     }
 ]
 // console.log(localStorage.setItem("tour", JSON.stringify(tour)));
 
-const txt = JSON.stringify(tours, undefined, 4)
-localStorage.setItem("tours", txt)
+// const txt = JSON.stringify(tours, undefined, 4)
+// localStorage.setItem("tours", txt)
+
+for (let i = 0; i < tours.length; i++) {
+    let heading = document.getElementById('heading')
+    let div1 = document.createElement('div')
+    div1.className = 'mb-5 d-flex'
+    let div2 = document.createElement('div')
+    div2.className = 'bg-warning border-danger border-top border-5 w-100 d-flex justify-content-center p-2'
+    let h2 = document.createElement('h2')
+    h2.className = 'text-white fs-5 fw-bold mb-0'
+    h2.innerText = tours[i].heading
+    div2.appendChild(h2)
+    div1.appendChild(div2)
+    heading.append(div1)
+    for (let j = 0; j < tours[i]['tours'].length; j++) {
+        let div1 = document.createElement('div')
+        div1.className = 'col'
+        let div2 = document.createElement('div')
+        div2.className = 'card'
+        div2.style.width = '18rem'
+        let img = document.createElement('img')
+        img.className = 'card-img-top'
+        img.src = tours[i]['tours'][j]['img']
+        let div3 = document.createElement('div')
+        div3.className = 'd-flex justify-content-space-around collapse'
+        div3.innerHTML = `<hr class='col'/>
+        <span class='btn d-inline border titleContent '>${tours[i]['tours'][j]['name']}</span>
+        <hr class='col '/>`
+        let div4 = document.createElement('div')
+        div1.className = 'card-body'
+        let ul = document.createElement('ul')
+        ul.innerHTML = `<li>${tours[i]['tours'][j]['price']}</li>
+                        <li>${tours[i]['tours'][j]['time']}</li>
+                        <li>${tours[i]['tours'][j]['vehicle']}</li>`
+        div4.appendChild(ul)
+        div2.appendChild(img)
+        div2.appendChild(div3)
+        div2.appendChild(div4)
+        div1.appendChild(div2)
+        heading.appendChild(div1)
+    }
+}
 
