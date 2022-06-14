51d4494049d57ea76a5bb1a5214ed0982cb91397
luongtruong20201 <luongtruong20201@gmail.com>
2022-06-14 11:31:23 +0700

    Sua file tour.js

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -1,4 +1,4 @@
-tour = [
+tours = [
     {title: "Hành trình Di sản Miền Trung", name: "Đà Nẵng - Hội An - Quảng Trị - Quảng Bình - Huế'", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô"},
     {title: "Đà Nẵng - Hội An - Cố đô Huế", name: "Đà Nẵng - Hội An - huế", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô"},
     {title: "Đà Nẵng - Đảo Lý Sơn", name: "Đà Nẵng - Đảo Lý Sơn", price: "Liên hệ để biết thêm", time: "2 Ngày 1 Đêm", vehicle: "Cano, Ô tô"},
@@ -6,11 +6,10 @@ tour = [
     {title: "Đà Nẵng - Mỹ Sơn - Hội An", name: "Đà Nẵng - Mỹ Sơn - Hội An", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô"},
     {title: "Đà Nẵng - Hội An - Vinpearl Land Hội An", name: "Đà Nẵng - Hội An - Bà Nà Hills - Vinpearl Land Nam Hội An", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô"},
     {title: "Đà Nẵng - Hội An - Núi Thần Tài", name: "Đà Nẵng - Hội An - Bà Nà Hills - Núi Thần Tài", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô"},
-    {title: "Đà Nẵng - Hội An - Cù Lao Chàm", name: "Đà Nẵng - Hội An - Bà Nà Hills - Cù Lao Chàm", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô"},
-    {title: "Đà Nẵng - Hội An - Bà Nà Hills", name: "Đà Nẵng - Hội An - Bà Nà Hills", price: "Liên hệ để biết thêm", time: "3 Ngày 2 Đêm", vehicle: "Ô tô"},
 ]
 
 // console.log(localStorage.setItem("tour", JSON.stringify(tour)));
 
-const txt = JSON.stringify(tour, undefined, 4)
-console.log(txt)
+const txt = JSON.stringify(tours, undefined, 4)
+localStorage.setItem("tours", txt)
+
