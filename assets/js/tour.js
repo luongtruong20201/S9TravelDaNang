88a207cab3a419d192979b81cf4a9ee47afc9f69
luongtruong20201 <luongtruong20201@gmail.com>
2022-06-14 11:26:35 +0700

    first commit

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -8,5 +8,9 @@ tour = [
     {title: "Đà Nẵng - Hội An - Núi Thần Tài", name: "Đà Nẵng - Hội An - Bà Nà Hills - Núi Thần Tài", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô"},
     {title: "Đà Nẵng - Hội An - Cù Lao Chàm", name: "Đà Nẵng - Hội An - Bà Nà Hills - Cù Lao Chàm", price: "Liên hệ để biết thêm", time: "4 Ngày 3 Đêm", vehicle: "Ô tô"},
     {title: "Đà Nẵng - Hội An - Bà Nà Hills", name: "Đà Nẵng - Hội An - Bà Nà Hills", price: "Liên hệ để biết thêm", time: "3 Ngày 2 Đêm", vehicle: "Ô tô"},
-
 ]
+
+// console.log(localStorage.setItem("tour", JSON.stringify(tour)));
+
+const txt = JSON.stringify(tour, undefined, 4)
+console.log(txt)
